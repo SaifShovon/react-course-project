f24f6d7fb6574c268a0e8fe0cd70d87092f390a8
SaifShovon <saaifshovon@gmail.com>
2019-09-16 23:21:04 +0600

    Manual Data Binding

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -53,43 +53,48 @@ var user = {
 
 var count = 0;
 var addOne = function addOne() {
-    console.log("AddOne");
+    count++;
+    renderCounterApp();
 };
 
 var minusOne = function minusOne() {
-    console.log("MinusOne");
+    count--;
+    renderCounterApp();
 };
 
 var reset = function reset() {
-    console.log("Reset");
+    count = 0;
+    renderCounterApp();
 };
-
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        'Count: ',
-        count
-    ),
-    React.createElement(
-        'button',
-        { onClick: minusOne },
-        '-1'
-    ),
-    React.createElement(
-        'button',
-        { onClick: addOne },
-        '+1'
-    ),
-    React.createElement(
-        'button',
-        { onClick: reset },
-        'Reset'
-    )
-);
-
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+var renderCounterApp = function renderCounterApp() {
+    var templateTwo = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Count: ',
+            count
+        ),
+        React.createElement(
+            'button',
+            { onClick: minusOne },
+            '-1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: addOne },
+            '+1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: reset },
+            'Reset'
+        )
+    );
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
