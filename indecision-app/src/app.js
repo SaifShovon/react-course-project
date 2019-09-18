0650f61116cf2b55da64195e19690fbb8efcca8e
SaifShovon <saaifshovon@gmail.com>
2019-09-18 22:45:22 +0600

    Picking an option

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -27,6 +27,13 @@ const  onRemoveAll = () =>{
     render();
 }
 
+const onMakeDecision = () =>{
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
+    console.log(randomNum);
+}
+
 const appRoot = document.getElementById('app');
 
 const numbers = [55,101,1000];
@@ -37,7 +44,7 @@ const render = () => {
             <h1>{app.title}</h1>
             {app.subtitle && <p>{app.subtitle}</p>}
             <p>{app.options.length > 0 ? "Here are your options " : "No Option"}</p>
-            <p>{app.options.length}</p>
+            <button disabled = {app.options.length === 0} onClick = {onMakeDecision}>What should i do?</button>
             <button onClick = {onRemoveAll}>Remove All</button>
             {
                 numbers.map((number) => {
