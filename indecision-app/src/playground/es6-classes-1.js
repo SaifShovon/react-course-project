d831123a6cdb4b9cc318ccd339399a97b6ca58a8
SaifShovon <saaifshovon@gmail.com>
2019-09-24 00:40:18 +0600

    Es6 Classe Part -2

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -1,28 +1,58 @@
 //setup constructor to take name and age (default to 0)
 //getDescription - Saif Shovon is 26 year old
 class Person {
-    // constructor(name = 'Annonymous'){
-    //     this.name = name;
-    // }
 
     constructor(name = 'Annonymous',age = 0){
-            this.name = name;
-            this.age = age;
+        this.name = name;
+        this.age = age;
      }
 
     getGretting(){
-       // return "Hi.."+ this.name + '!!';
        return `Hi. I am ${this.name}! `;
     }
     getDescription(){
-            // return "Hi.."+ this.name + '!!';
-            return ` ${this.name} is ${this.age} year's old`;
+        return ` ${this.name} is ${this.age} year's old.`;
     }
 }
 
-const me = new Person('Saif Shovon');
+class Student extends Person{
+        constructor(name,age,major = "CSE"){
+                super(name,age);
+                this.major = major;
+        }
+        hasMajor(){
+                return !!this.major;
+        }
+
+        getDescription(){
+                let description = super.getDescription();
+                if(this.hasMajor()){
+                        description += ` His Major is ${this.major}`;
+                }
+                return description;
+        }
+}
+
+class Traveller extends Person{
+        constructor(name,age,homeLocations){
+                super(name,age);
+                this.homeLocations = homeLocations;
+        }
+
+        getGretting(){
+                let gretting = super.getGretting();
+                if(this.homeLocations){
+                        gretting += `I am visiting from ${this.homeLocations}`;
+                }
+                return gretting;
+        }
+}
+
+const me = new Student('Saif Shovon');
 console.log(me.getGretting());
 
-const other = new Person("saif shovon",26);
-console.log(other.getGretting());
- console.log(other.getDescription());
+const other = new Student("saif shovon",26,"BBA");
+console.log(other.getDescription());
+
+const traveller = new Traveller("saif shovon",26,"Magura");
+console.log(traveller.getGretting());
